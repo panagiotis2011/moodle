a59899a1d6db9fbe36a2d8aa5d4c6ab063eef47e
panagiotis2011 <sch@sch.gr>
2011-12-05 23:03:49 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moodle::Application.config.secret_token = 'd963b65f29b9e8cc7a62b4cdb9acd2087d242d67f1f0b79a34afb5dccfc9dee4e2c497c8bd0ca4e1adca1e53c8fd8da8a0d6f4d5df4e379650529e7d35a9c91f'
